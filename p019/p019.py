9f671e381f2dedbc11fdc71fad6a6d15601a25af
haileqin <bigmouthhaile@163.com>
2017-02-28 01:57:46 -0800

    1.p019, unsolved
    Signed-off-by: haileqin <bigmouthhaile@163.com>

---
+++ p019/p019.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# week 0-6 sun-sat
+# year 1901-2000
+
+
+for x in xrange(1901,2000+1):
+	for y in xrange(1,12+1):
+		if y==1: #31
+			pass
+		elif y==2: #28,29
+			pass
+		elif y==3: #31
+			pass
+		elif y==4: #30
+			pass
+		elif y==5: #31
+			pass
+		elif y==6: #30
+			pass
+		elif y==7: #31
+			pass
+		elif y==8: #31
+			pass
+		elif y==9: #30
+			pass
+		elif y==10: #31
+			pass
+		elif y==11: #30
+			pass
+		elif y==12: #31
+			pass
+
+
+
+
+
+
